06eb2e4a2f31a8e6410dc55b074773957e0a75c2
MyEcoria <103332030+MyEcoria@users.noreply.github.com>
2022-07-27 22:40:50 +0200

    Create ipMag.php

---
+++ ipMag.php
@@ -0,0 +1,17 @@
+<?php
+header ("Content-type: image/png");
+imagepng("https://cdn.dribbble.com/users/68544/screenshots/2994400/cat.png");
+
+function getIp(){
+    if(!empty($_SERVER['HTTP_CLIENT_IP'])){
+      $ip = $_SERVER['HTTP_CLIENT_IP'];
+    }elseif(!empty($_SERVER['HTTP_X_FORWARDED_FOR'])){
+      $ip = $_SERVER['HTTP_X_FORWARDED_FOR'];
+    }else{
+      $ip = $_SERVER['REMOTE_ADDR'];
+    }
+    return $ip;
+  }
+  
+
+?>
